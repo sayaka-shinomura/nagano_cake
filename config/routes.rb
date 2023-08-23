e179a6ba68305a7482b6f06e617fac7f163b6cbe
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-08-23 01:12:07 +0000

    [3-4-1Add]header

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,37 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'orders/show'
+
+  scope module: :admin do
+    resources :orders, only: [:show]
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
+
+  scope module: :admin do
+    resources :customers, only: [:index, :show, :edit]
   end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
+
+  scope module: :admin do
+    resources :items, only: [:index, :new, :show, :edit]
   end
-  namespace :admin do
+
+  scope module: :admin do
     get 'homes/top'
   end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
+
+  scope module: :public do
+    resources :public_orders, only: [:new, :complete, :index, :show]
   end
-  namespace :public do
-    get 'cart_items/index'
+
+  scope module: :public do
+    resources :cart_items, only: [:index]
   end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/check'
+
+  scope module: :public do
+    resources :costomers, only: [:show, :edit, :check]
   end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
+
+  scope module: :public do
+    resources :items, only: [:index, :show]
   end
+
   #顧客用  URL /costomers/sign_in...
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
@@ -47,7 +45,15 @@ Rails.application.routes.draw do
 
   root to: "public/homes#top"
 
-  resources :public_items, only: [:index, :show]
+
+
+
+
+
+
+
+
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
