e8414995efc1c51e5f5ab8c9fcb94a8c322b0b73
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-08-23 02:16:08 +0000

    [3-4-1Add]sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+
+  #管理者のサインイン後の遷移先
+  def after_admin_sign_in_path_for(resource)
+    admin_items_path
+  end
+
+  #管理者のサインアウト後の遷移先
+  def after_admin_sign_out_path_for(resource)
+    admin_sign_in_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password])
+  end
+
 end
