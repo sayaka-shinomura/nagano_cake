790b5a16ba8fff0f38ddedb12ec245f262215914
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-09-02 11:17:18 +0000

    [Add]admin_customer_show

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,6 +4,8 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    @customers = Customer.all
   end
 
   def edit
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -22,4 +22,9 @@ class Customer < ApplicationRecord
     self.last_name + " " + self.first_name
   end
 
+  #姓と名を一列で表示(カナ)
+  def full_name_kana
+    self.last_name_kana + " " + self.first_name_kana
+  end
+
 end
