7fcec115631c1c389666e39c9a81319e07318e22
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-09-03 02:07:27 +0000

    [Add]customers_items_show

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,7 @@
 class Public::ItemsController < ApplicationController
 
   def index
-     @items = Item.page(params[:page]).per(4)
+     @items = Item.page(params[:page]).per(8)
      @item = Item.all
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,5 +9,9 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  def add_tax_price
+    (self.price * 1.08).round
+  end
+
 
 end
