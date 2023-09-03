3cc18257b759fee7ae8f7ef760df3b8ee5a1af9c
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-09-03 01:00:39 +0000

    [Add]customers_top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,11 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @items = Item.all.order(id: "DESC").page(params[:page]).per(4)
+    @item = Item.new
+    # 新着商品を上から4件取得
+    @items_latest4 = Item.order(created_at: :desc).first(4)
+    # 新着商品4件を除く全商品を取得
+    @items_offset4 = Item.order(created_at: :desc).offset(4)
   end
 
   def about
