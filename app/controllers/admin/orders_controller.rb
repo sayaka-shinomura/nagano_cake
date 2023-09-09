c1a031e6aa76ed5d16d7500aad312eb5274adae8
sayaka-shinomura <138592145+sayaka-shinomura@users.noreply.github.com>
2023-09-09 05:59:07 +0000

    [Add]admin_orders_show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,8 +2,9 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_details = @order.order_details.all
+    @order_details = @order.order_details
     @total_payment = @order.billing_amount - @order.postage
   end
+
 
 end
